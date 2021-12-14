048c5a986b7e178b0bd7e4530a0f35fe18317553
Nelson ortiz <nelso570@gmail.com>
2021-12-14 12:52:36 -0500

    Calculadora React PWA

---
+++ src/components/Calculadora.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useState } from 'react';
+import NumberInput from './NumberInput';
+
+
+const Calculadora = () => {
+    
+
+
+    return (
+        <div>
+            <NumberInput />
+            
+        
+        </div>
+    )
+}
+
+export default Calculadora
